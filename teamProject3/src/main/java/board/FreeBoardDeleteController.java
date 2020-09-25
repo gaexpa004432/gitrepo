9dd03452285c0bcbc990133e2914bea202a7d33c
'seosongmi' <'sm-seo2000@hanmail.net'>
2020-09-25 18:05:32 +0900

    9/25

---
+++ teamProject3/src/main/java/board/FreeBoardDeleteController.java
@@ -0,0 +1,25 @@
+package board;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import controller.Controller;
+import model.FreeBoardDAO;
+import model.FreeBoardVO;
+
+public class FreeBoardDeleteController implements Controller {
+
+	public void execute(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		int board_no=Integer.parseInt(request.getParameter("board_no"));
+		FreeBoardVO freeboard = new FreeBoardVO();
+		freeboard.setBoard_no(board_no);
+		FreeBoardDAO DAO = new FreeBoardDAO();
+		DAO.delete(freeboard);
+		
+		//forward
+	    request.getRequestDispatcher("/freeBoardList.do").forward(request, response);
+	}
+}
