a315b9fa93cf87475abe75a1e29af544ae2ee464
admin <gaexpa004432@naver.com>
2020-10-07 09:27:02 +0900

    장바구니

--- teamProject3/src/main/java/buy/CartController.java
+++ teamProject3/src/main/java/buy/CartController.java
@@ -1,6 +1,8 @@
 package buy;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
@@ -12,8 +14,19 @@ public class CartController implements Controller {
 
 	public void execute(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		String[] recipe_number =  request.getParameterValues("product_number");
-		 //for(String str:recipe_number) { System.out.println(str); }
-		request.getSession().setAttribute("product",recipe_number);
+		List<String[]> list = null;
+		if((List<String[]>)request.getSession().getAttribute("product") == null) { //현재 세션에 장바구니가 저장되어있는지 체크
+			list = new ArrayList<String[]>();	
+		}else {
+			list =(List<String[]>)request.getSession().getAttribute("product"); // 저장되어있다면 list 변수에 들고옴
+		} 
+		list.add(recipe_number); // 장바구니 리스트에 현재 장바구니 추가
+		
+		for(String[] str : list) {
+			System.out.println(str[0]); // 확인용
+		}
+		request.getSession().setAttribute("product",list); // 컬렉션안에 배열이 저장된형태
+		
 	}
 
 }
